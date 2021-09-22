9aa5c906434c1120e2b92cc5578a314d439e712d
nallera-meli <nicolas.allera@mercadolibre.com>
2021-09-22 10:45:05 -0300

    Added Emi

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello_world():
 
 @app.route('/random-list', methods=['GET', 'POST'])
 def randomlist():
-    l = ['Clau', 'Tonny', 'Lucho', 'Facu', 'Luis', 'Nico']
+    l = ['Clau', 'Tonny', 'Lucho', 'Facu', 'Luis', 'Nico', 'Emi']
     random.shuffle(l)
     msg = ""
     counter = 1
