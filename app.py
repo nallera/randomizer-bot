cda155997cb491ae0185edf900d3b9ee46fecece
nallera-meli <nicolas.allera@mercadolibre.com>
2022-05-09 09:57:37 -0300

    agregue adolpho

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello_world():
 
 @app.route('/random-list', methods=['GET', 'POST'])
 def randomlist():
-    l = ['Clau', 'Tonny', 'Facu', 'Luis', 'Nico', 'Emi', 'Flavio']
+    l = ['Clau', 'Tonny', 'Facu', 'Luis', 'Nico', 'Emi', 'Flavio', 'Adolpho']
     random.shuffle(l)
     msg = ""
     counter = 1
